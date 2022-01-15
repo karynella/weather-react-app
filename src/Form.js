8bb66e98f1b0d6100c6b1baaf7626baddbd09258
karynella <karynella@live.com>
2022-01-15 13:44:45 +0000

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -30,7 +30,7 @@ export default function searchEngine() {
             alt = "" / >
             <
             /p>{" "} <
-            /ul> <
+            /ul>{" "} <
             /div>
         );
     }
