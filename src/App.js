111367c4734ca2e532a8f4befe4f0af7d095e45d
karynella <karynella@live.com>
2022-01-16 18:59:53 +0000

    Fixed code
    code fixed

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import "./App.css";
 import Form from "./Form.js";
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        h1 > Weather App < /h1>{" "} <
-        /header>
-
-        <
-        Form / >
-        <
-        /div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Weather App </h1>
+      </header>
+      <Form />
+    </div>
+  );
 }
 
 export default App;
